0c675bc64720d83b340bfe08cc4f1e3eb7e04e61
Dari <Dari@dari.com>
2020-06-29 23:16:34 -0400

    fixing bugs and supporting new resolutions

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,22 @@ use webbrowser;
 use enigo::*;
 use std::thread::sleep_ms;
 use chrono::{DateTime, Utc};
+use std::io::stdin;
 
 fn main() {
+    let mut size = String::new();
     println!("Dari's Overwatch League watcher. Special thanks to https://gigabra.in/ for providing a platform that actually gives tokens instead of this inconsistent mess of a \"system\" that is overwatchleague.com\nSource code at https://github.com/Dari420/overwatch-league-watcher");
+    loop {
+        if size != String::from("100") || size != String::from("125") {
+            println!("Please enter your windows scale (100 or 125)");
+            stdin()
+                .read_line(&mut size)
+                .expect("error reading line, aborting");
+        }
+        else {
+            break;
+        }
+    }
     loop {
         let current_time: DateTime<Utc> = Utc::now();
         println!("UTC now is: {}", current_time.format("%a %b %e %T %Y"));
@@ -17,31 +30,80 @@ fn main() {
         time.truncate(5);
         time = time.chars().rev().collect::<String>();
         if date == String::from("Sat") && time == String::from("08:05") || time == String::from("18:35") {
-            open_gigabrain_overwatch();
+            open_gigabrain_overwatch(size);
             break;
         }
         else if date == String::from("Sun") && time == String::from("08:05") || time == String::from("18:35") {
-            open_gigabrain_overwatch();
+            open_gigabrain_overwatch(size);
             break;
         }
         sleep_ms(60000);
     }
 }
 
-fn open_gigabrain_overwatch() {
+fn open_gigabrain_overwatch(size: String) {
     let mut enigo = Enigo::new();
     println!("Opening overwatch league.");
-    if webbrowser::open("https://gigabra.in").is_ok() {
-        enigo.mouse_move_to(1300, 260);
-        sleep_ms(2000);
-        enigo.mouse_down(MouseButton::Left);
-        enigo.mouse_up(MouseButton::Left);
-        enigo.mouse_move_to(700, 600);
-        sleep_ms(6500);
-        enigo.mouse_down(MouseButton::Left);
-        enigo.mouse_up(MouseButton::Left);
-        sleep_ms(3000);
-        enigo.mouse_down(MouseButton::Left);
-        enigo.mouse_up(MouseButton::Left);
+    if size == String::from("100") {
+        if webbrowser::open("https://gigabra.in").is_ok() {
+            //Clicks on every possible login location starting from most likely to least likely
+            enigo.mouse_move_to(1300, 260);
+            sleep_ms(2000);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            enigo.mouse_move_to(1300, 243);
+            sleep_ms(200);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            enigo.mouse_move_to(1300, 150);
+            sleep_ms(200);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //waits for the disclaimer page to allow the button to be clicked, then clicks it
+            enigo.mouse_move_to(700, 600);
+            sleep_ms(6500);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //clicks allow button
+            enigo.mouse_move_to(600, 700);
+            sleep_ms(1500);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //plays video
+            sleep_ms(3000);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+        }
+    }
+    else {
+        if webbrowser::open("https://gigabra.in").is_ok() {
+            //Clicks on every possible login location starting from most likely to least likely
+            enigo.mouse_move_to(1300, 260);
+            sleep_ms(2000);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            enigo.mouse_move_to(1300, 243);
+            sleep_ms(200);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            enigo.mouse_move_to(1300, 150);
+            sleep_ms(200);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //waits for the disclaimer page to allow the button to be clicked, then clicks it
+            enigo.mouse_move_to(700, 600);
+            sleep_ms(6500);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //clicks allow button
+            enigo.mouse_move_to(600, 700);
+            sleep_ms(1500);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+            //plays video
+            sleep_ms(3000);
+            enigo.mouse_down(MouseButton::Left);
+            enigo.mouse_up(MouseButton::Left);
+        }
     }
 }
